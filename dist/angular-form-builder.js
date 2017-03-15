9754e4516b79b029c6d0f393d1ef371a35b64da5
Jeremy Kerr-Wilson <jeremy@microquest.ca>
2017-03-15 13:07:03 -0600

    Fixe test

--- dist/angular-form-builder.js
+++ dist/angular-form-builder.js
@@ -2086,7 +2086,7 @@
             var _results1;
             _results1 = [];
             for (component in formRows[row].formObjects) {
-              console.log("adding " + name(+' ' + row));
+              console.log("adding " + name + ", " + row);
               _results1.push(this.addFormObject(name, row, formRows[row].formObjects[component]));
             }
             return _results1;
